8098bd2f6c43ef711332b86eb26c9707b74b4672
Avery <awix1@yahoo.com>
2022-08-21 19:49:27 -0400

    winnign and lose

--- Source/ToonTanks/ToonTanksGameMode.cpp
+++ Source/ToonTanks/ToonTanksGameMode.cpp
@@ -22,17 +22,28 @@ void AToonTanksGameMode::ActorDied(AActor * DeadActor)
         {    
             ToonTanksPlayerController->SetPlayerEnabledState(false);
         }   
+        GameOver(false);
     }
     else if (ATower * DestroyedTower = Cast<ATower>(DeadActor))
     {
         DestroyedTower->HandleDestruction();
+        --TargetTowers;
+
+        if (TargetTowers == 0)
+        {
+            GameOver(true);
+        }
     }
 }
 
 void AToonTanksGameMode::HandleGameStart()
 {
+    TargetTowers = GetTargetTowerCount();
+
     Tank = Cast<ATank>(UGameplayStatics::GetPlayerPawn(this, 0));
     ToonTanksPlayerController = Cast<AToonTanksPlayerController>(UGameplayStatics::GetPlayerController(this, 0));
+
+    StartGame();
 
     if (ToonTanksPlayerController)
     {
@@ -51,9 +62,14 @@ void AToonTanksGameMode::HandleGameStart()
             StartDelay, 
             false
         );
-        
-
     }
+}
+
+int32 AToonTanksGameMode::GetTargetTowerCount()
+{
+    TArray<AActor *> Towers;     
+    UGameplayStatics::GetAllActorsOfClass(this, ATower::StaticClass(), Towers);
+    return Towers.Num();
 }
 
 
